db73a8cfe823f2915283ab213140c4c56fb463a0
Peter Philipp <peter.philipp@cando-image.com>
2022-10-03 13:57:14 +0200

    chore: Add caching to ClassTypeDefinitions build.

--- src/EventListener/DataChangeListener.php
+++ src/EventListener/DataChangeListener.php
@@ -16,11 +16,15 @@
 namespace Pimcore\Bundle\DataHubBundle\EventListener;
 
 use Pimcore\Bundle\DataHubBundle\Configuration;
+use Pimcore\Bundle\DataHubBundle\GraphQL\ClassTypeDefinitions;
 use Pimcore\Bundle\DataHubBundle\WorkspaceHelper;
+use Pimcore\Cache;
 use Pimcore\Event\AssetEvents;
+use Pimcore\Event\DataObjectClassDefinitionEvents;
 use Pimcore\Event\DataObjectEvents;
 use Pimcore\Event\DocumentEvents;
 use Pimcore\Event\Model\AssetEvent;
+use Pimcore\Event\Model\DataObject\ClassDefinitionEvent;
 use Pimcore\Event\Model\DataObjectEvent;
 use Pimcore\Event\Model\DocumentEvent;
 use Pimcore\Model\Element\ValidationException;
@@ -40,6 +44,9 @@ class DataChangeListener implements EventSubscriberInterface
             DocumentEvents::POST_DELETE => 'onDocumentDelete',
             AssetEvents::POST_UPDATE => 'onAssetUpdate',
             AssetEvents::POST_DELETE => 'onAssetDelete',
+            DataObjectClassDefinitionEvents::POST_ADD => 'onClassDefinitionAdded',
+            DataObjectClassDefinitionEvents::POST_UPDATE => 'onClassDefinitionUpdated',
+            DataObjectClassDefinitionEvents::POST_DELETE => 'onClassDefinitionDeleted',
         ];
     }
 
@@ -164,5 +171,20 @@ class DataChangeListener implements EventSubscriberInterface
                 throw new ValidationException(sprintf('Could not save configuration: %s', $e->getMessage()), 0, $e);
             }
         }
+    }
+
+    public function onClassDefinitionAdded(ClassDefinitionEvent $event)
+    {
+        ClassTypeDefinitions::getClasses(true);
+    }
+
+    public function onClassDefinitionUpdated(ClassDefinitionEvent $event)
+    {
+        ClassTypeDefinitions::getClasses(true);
+    }
+
+    public function onClassDefinitionDeleted(ClassDefinitionEvent $event)
+    {
+        ClassTypeDefinitions::getClasses(true);
     }
 }
--- src/GraphQL/ClassTypeDefinitions.php
+++ src/GraphQL/ClassTypeDefinitions.php
@@ -18,6 +18,7 @@ namespace Pimcore\Bundle\DataHubBundle\GraphQL;
 use Pimcore\Bundle\DataHubBundle\Configuration;
 use Pimcore\Bundle\DataHubBundle\GraphQL\DataObjectType\PimcoreObjectType;
 use Pimcore\Bundle\DataHubBundle\GraphQL\Exception\ClientSafeException;
+use Pimcore\Cache;
 use Pimcore\Cache\Runtime;
 use Pimcore\Db;
 use Pimcore\Model\DataObject\ClassDefinition;
@@ -30,15 +31,39 @@ class ClassTypeDefinitions
     public static $definitions = [];
 
     /**
+     * Returns list of currently defined classes.
+     *
+     * This is executed very often while having very rarely changes.
+     * Avoid triggering a full DB query every time and use cache instead.
+     *
+     * @see DataChangeListener::onClassDefinitionAdded()
+     * @see DataChangeListener::onClassDefinitionUpdated()
+     * @see DataChangeListener::onClassDefinitionDeleted()
+     *
+     * @param bool $skipCache
+     *
+     * @return array
+     */
+    public static function getClasses(bool $skipCache = false): array
+    {
+        // __METHOD__ has characters the caching handler doesn't like in an ID.
+        $cid = md5(__METHOD__);
+        if ($skipCache || !is_array(($listing = Cache::load($cid)))) {
+            $db = Db::get();
+            $listing = $db->fetchAll('select id, name from classes');
+            // Can't use __METHOD__ as tag because of invalid chars.
+            Cache::save($listing, $cid, ['ClassTypeDefinitions', 'data-hub']);
+        }
+        return $listing;
+    }
+
+    /**
      * @param Service $graphQlService
      * @param array $context
      */
     public static function build(Service $graphQlService, $context = [])
     {
-        $db = Db::get();
-        $listing = $db->fetchAll('select id, name from classes');
-
-        foreach ($listing as $class) {
+        foreach (self::getClasses() as $class) {
             $id = $class['id'];
             $name = $class['name'];
             $objectType = new PimcoreObjectType($graphQlService, $name, $id, [], $context);
