266f8a4422d752efa1f04508c78f20f21516aac9
Julian Herzig <julian.herzig91@gmail.com>
2023-04-06 10:07:14 +0200

    feat: support filters in fragments

--- src/GraphQL/Resolver/QueryType.php
+++ src/GraphQL/Resolver/QueryType.php
@@ -966,6 +966,16 @@ class QueryType
         $requestedFilters = $resolveInfo->operation->selectionSet->selections[0]->selectionSet->selections[0]->selectionSet->selections;
 
         foreach ($requestedFilters as $filter) {
+            if ($filter instanceof FragmentSpreadNode) {
+                $filterName = $filter->name->value;
+                $fragmentSelectionSet = $resolveInfo->fragments[$filterName]->selectionSet->selections;
+                foreach ($fragmentSelectionSet as $fragmentSelection) {
+                    if (isset($fragmentSelection->alias) && $fragmentSelection->alias->value == $facetName) {
+                        $filterNodes = $fragmentSelection->selectionSet->selections;
+                    } 
+                }
+                break;
+            }
             if (isset($filter->alias) && $filter->alias->value == $facetName) {
                 $filterNodes = $filter->selectionSet->selections;
             }
