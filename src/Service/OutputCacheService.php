aaf63423e5cc551e0297c38f9fe42bc8fdff5d28
joherzig <joherzig@users.noreply.github.com>
2023-05-09 07:15:17 +0000

    Apply php-cs-fixer changes

--- src/Service/OutputCacheService.php
+++ src/Service/OutputCacheService.php
@@ -28,8 +28,8 @@ use Pimcore\Logger;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
 
 class OutputCacheService
 {
