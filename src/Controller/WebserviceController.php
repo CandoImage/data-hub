8c4edb1d748b251368dd2b5f22d1d2c71f254744
Peter Philipp <peter.philipp@cando-image.com>
2024-04-08 16:02:47 +0200

    feat: Add possibility to have cache item isolation.

--- src/Controller/WebserviceController.php
+++ src/Controller/WebserviceController.php
@@ -95,7 +95,7 @@ class WebserviceController extends FrontendController
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      *
-     * @return array{operations: bool, resolveEdge: bool, resolveObjectGetter: bool}
+     * @return array{operations: bool, resolveEdge: bool, resolveObjectGetter: bool, cidBase: string}
      */
     protected function getCachingContextConfiguration(Request $request): array
     {
@@ -103,6 +103,9 @@ class WebserviceController extends FrontendController
             'operations' => true,
             'resolveEdge' => true,
             'resolveObjectGetter' => true,
+            // Allows to inject global contexts into the cache handling,
+            // isolating cache items by custom rules.
+            'cidBase' => '',
         ];
         $env = Config::getEnvironment();
         if (!in_array(strtolower($env), ['prod', 'production'])) {
